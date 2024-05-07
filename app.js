ddec21feaca3729f14ad5237928655252097f5af
Munkhbold <munkhbold88d@gmail.com>
2024-05-07 16:29:40 -0600

    w3-d2-lab-finished

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import sample from 'lodash.sample';
 import express from 'express';
 import morgan from 'morgan';
 import nunjucks from 'nunjucks';
@@ -48,5 +49,33 @@ app.get('/hello', (req, res) => {
 // Handle the form from /hello and greet the user.
 app.get('/greet', (req, res) => {
   const name = req.query.name || 'stranger';
-  res.render('greet.html.njk', { name: name });
+  const compliment = sample(COMPLIMENTS);
+  res.render('greet.html',{
+    name: name,
+    compliment: compliment
+  });
 });
+
+app.get('/game', (req, res) => {
+  const play = req.query.play;
+
+  if (play === 'no') {
+    return res.render('goodbye.html', {message: "Goodbye! You'll be missed"});
+      } else if (play === 'yes'){
+        res.render('game.html');
+      }
+});
+
+app.post('/madlib', (req, res) => {
+  const person = req.body.person;
+  const color = req.body.color;
+  const noun = req.body.noun;
+  const adjective = req.body.adjective;
+
+  res.render('madlib.html', {person, color, noun, adjective});
+});
+
+
+
+
+
